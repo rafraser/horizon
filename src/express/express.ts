7a6d3de65d14687924101b6dc24ddc62ef4473be
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-01-09 01:26:23 +1000

    Login gate middleware

--- src/express/express.ts
+++ src/express/express.ts
@@ -1,8 +1,11 @@
 import express, { Application } from 'express'
+import path from 'path'
+
 import { HorizonClient } from '../discord/horizon'
 
 import configuredSession from './session'
 import userRoutes from './routes/user'
+import { requireLogin } from './middleware'
 
 export interface ExpressServer extends Application {
   horizonClient?: HorizonClient
@@ -10,6 +13,7 @@ export interface ExpressServer extends Application {
 
 export const app = express() as ExpressServer
 app.use(configuredSession)
+app.use(express.static('assets'))
 
 app.get('/', (req, res) => {
   if (req.session.user) {
@@ -17,6 +21,10 @@ app.get('/', (req, res) => {
   } else {
     res.send('Hello World!')
   }
+})
+
+app.get('/test', requireLogin, (req, res) => {
+  res.send('You have reached the super secret page. Congratulations.<br>' + req.session.loginRedirect + '<br><br><br>' + JSON.stringify(req.session.user))
 })
 
 app.listen(process.env.EXPRESS_PORT, () => {
--- src/express/middleware.ts
+++ src/express/middleware.ts
@@ -2,9 +2,9 @@ import express from 'express'
 
 export function requireLogin (req: express.Request, res: express.Response, next: express.NextFunction) {
   if (!req.session.user) {
-    req.session.loginRedirect = req.baseUrl
+    req.session.loginRedirect = req.originalUrl
     req.session.save(_ => {
-      res.redirect('/user/login')
+      res.sendFile('pages/login.html', { root: '.' })
     })
 
     return
