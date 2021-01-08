03146117ae86ab7a94504d041f45eb2971185909
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-01-09 03:47:56 +1000

    Close unused game rooms

--- src/room.ts
+++ src/room.ts
@@ -7,6 +7,9 @@ import configuredSession from './express/session'
 
 const wrap = (middleware: any) => (socket: Socket, next: NextFunction) => middleware(socket.request, {}, next)
 
+export const roomList = new Map() as Map<string, GameRoom>
+export const userPlaying = new Map() as Map<string, boolean>
+
 export interface GameroomType {
   page: string
   register: (socket: Socket, room: GameRoom) => void
@@ -18,6 +21,8 @@ export class GameRoom {
   type: GameroomType;
   io: Namespace;
   clients: number;
+  // eslint-disable-next-line no-undef
+  timeout?: NodeJS.Timeout;
 
   constructor (type: GameroomType, host: string = undefined) {
     this.type = type
@@ -25,8 +30,8 @@ export class GameRoom {
 
     this.id = Math.random().toString(16).slice(2) // can we do better
     this.clients = 0
-    console.log('New GameRoom!', this.id)
 
+    console.log('New GameRoom!', this.id)
     this.io = io.of('/' + this.id)
 
     // Don't let sockets connect without authentication
@@ -40,10 +45,35 @@ export class GameRoom {
       }
     })
 
+    // Handle socket connections - pass to roomtype handler
     this.io.on('connection', socket => {
       this.clients++
+      userPlaying.set(socket.user.id, true)
+
+      if (this.timeout) {
+        clearTimeout(this.timeout)
+      }
+
+      // Delete rooms with no players
+      socket.on('disconnect', () => {
+        this.clients--
+        if (this.clients <= 0) {
+          this.remove()
+        }
+      })
 
       type.register(socket, this)
     })
+
+    // Add to the room list
+    roomList.set(this.id, this)
+
+    // Add a timeout for the first connection
+    this.timeout = setTimeout(() => this.remove(), 30 * 1000)
+  }
+
+  public remove () {
+    console.log('Closing gameroom!', this.id)
+    roomList.delete(this.id)
   }
 }
