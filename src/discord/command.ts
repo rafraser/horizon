6f581a59f0b3c528c870ebc77a90cab181d4b5cd
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2021-02-20 21:05:30 +1000

    Copy some utility functions from Bottimus

--- src/discord/command.ts
+++ src/discord/command.ts
@@ -1,5 +1,5 @@
 import { HorizonClient } from './horizon'
-import { Message } from 'discord.js'
+import { GuildMember, Message } from 'discord.js'
 
 export interface Command {
   name: string
@@ -8,6 +8,51 @@ export interface Command {
   cooldown?: number
 
   execute: (client: HorizonClient, message: Message, args: string[]) => void
+}
+
+export async function findUser (message: Message, args: string[], retself: boolean = false): Promise<GuildMember> {
+  // Return mentioned user if any were in the message
+  if (message.mentions.members.size >= 1) {
+    return message.mentions.members.first()
+  }
+
+  // Handle case with 0 arguments
+  if (!args || args.length < 1) {
+    if (retself) {
+      return message.member
+    } else {
+      throw new Error('No user found!')
+    }
+  }
+
+  // Fetch the guild members if it's not cached for some reason
+  if (message.guild.members.cache.size <= 2) {
+    await message.guild.members.fetch()
+  }
+
+  // Search the list of users for matching names
+  const search = args.shift().toLowerCase()
+  const results = message.guild.members.cache.filter(u => {
+    return u.displayName.toLowerCase().includes(search) ||
+            u.user.username.toLowerCase().includes(search) ||
+            u.user.tag.toLowerCase() === search
+  })
+
+  // Return results or raise an error
+  // In the event no user was found, shove the argument back on the list
+  if (results.size > 1) {
+    throw new Error('More than one user matched!')
+  } else if (results.size < 1) {
+    if (retself) {
+      args.unshift(search)
+      return message.member
+    } else {
+      args.unshift(search)
+      throw new Error('No user found!')
+    }
+  } else {
+    return results.first()
+  }
 }
 
 // Export these along so we can do a neat import in command implementations
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import { promisify } from 'util'
+import { spawn } from 'child_process'
 export const readdirAsync = promisify(fs.readdir)
 
 export class User {
@@ -7,4 +8,35 @@ export class User {
   username: string;
   discriminator: string;
   avatar: string;
+}
+
+export async function executePython (script: string, args: string[]): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const pythonCommand = 'python'
+    let pythonInstance
+
+    // Handle args and spawn the python instance
+    if (args) {
+      const argsArray = Array.isArray(args) ? args : [args]
+      argsArray.unshift('util/' + script + '.py')
+      pythonInstance = spawn(pythonCommand, args)
+    } else {
+      pythonInstance = spawn(pythonCommand, ['util/' + script + '.py'])
+    }
+
+    // Capture output
+    let data = ''
+    pythonInstance.stdout.on('data', d => { data += d })
+    pythonInstance.stderr.on('data', d => { data += d })
+
+    // Resolve/reject the promise depending on exit code
+    pythonInstance.on('close', code => {
+      data = data.trim()
+      if (code === 0) {
+        resolve(data)
+      } else {
+        reject(data)
+      }
+    })
+  })
 }
